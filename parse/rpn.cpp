61d7c36c8922f950e706a466ff4b4381859d5b2b
Jacob <jelondon12@gmail.com>
2021-05-31 10:59:32 -0700

    working on more generic math ops

--- parse/rpn.cpp
+++ parse/rpn.cpp
@@ -30,44 +30,21 @@ enum Symbol {
     SYM_SUB,
     SYM_MULT,
     SYM_DIV,
-    SYM_COUNT,
+    SYM_COUNT
 };
 
+// order of precedence, lowest to highest
 enum Type {
-    TYPE_FLOAT,
-    TYPE_UINT,
     TYPE_INT,
+    TYPE_UINT,
+    TYPE_FLOAT,
+    TYPE_COUNT
 };
 
 union Number {
-    Float f;
     Int i;
     Uint u;
-};
-
-//struct Number {
-//    virtual void add(Number& other) = 0;
-//    virtual void add(Number& other) = 0;
-//    virtual void add(Number& other) = 0;
-//
-//    virtual void sub(Number& other) = 0;
-//    virtual void div(Number& other) = 0;
-//    virtual void mul(Number& other) = 0;
-//};
-//
-//struct NumberFloat : public Number {
-//    Float value;
-//    NumberFloat add(Float value) override;
-//    NumberFloat add(Int value) override;
-//    NumberFloat add(Uint value) override;
-//
-//    void sub(Number& other) override;
-//    void div(Number& other) override;
-//    void mul(Number& other) override;
-//};
-
-struct Num {
-
+    Float f;
 };
 
 struct Value {
@@ -75,9 +52,14 @@ struct Value {
     Type type;
 
     Value() noexcept;
-    Value(Float number) noexcept;
     Value(Int number) noexcept;
     Value(Uint number) noexcept;
+    Value(Float number) noexcept;
+
+    void print() noexcept;
+    enum Type coerce_chk(Value& other) noexcept; // greatest precedence
+    void coerce_exec(enum Type type) noexcept; // cast to the type
+    void coerce(Value& other) noexcept;
 
     Value add(Value& other) noexcept;
     Value sub(Value& other) noexcept;
@@ -85,14 +67,29 @@ struct Value {
     Value mul(Value& other) noexcept;
 };
 
+// assume This and Other are the same type
+#define value_oneop(ThisP, Other, Op) \
+    do { \
+        switch (ThisP->type) { \
+        case TYPE_FLOAT: return Value(this->number.f Op other.number.f); \
+        case TYPE_INT:   return Value(this->number.i Op other.number.i); \
+        case TYPE_UINT:  return Value(this->number.u Op other.number.u); \
+        default: \
+            assert(0); \
+            break; \
+        } \
+    } while (0)
+
 struct Node {
     virtual void exec(std::stack<Value>& stack) noexcept = 0;
 };
 
-struct SymNode : public Node {
-    Symbol value;
+typedef void (* SymOp)(std::stack<Value>& stack);
 
-    SymNode(Symbol value) noexcept;
+struct SymNode : public Node {
+    SymOp op;
+
+    SymNode(SymOp op) noexcept;
     void exec(std::stack<Value>& stack) noexcept override;
 };
 
@@ -106,19 +103,25 @@ struct NumNode : public Node {
 static Node *node_new(char *value) noexcept;
 static void node_free(Node *self) noexcept;
 
-typedef void (* SymOp)(std::stack<Value>& stack);
 static void op_none(std::stack<Value>& stack) noexcept;
 static void op_add(std::stack<Value>& stack) noexcept;
 static void op_sub(std::stack<Value>& stack) noexcept;
-static void op_mult(std::stack<Value>& stack) noexcept;
+static void op_mul(std::stack<Value>& stack) noexcept;
 static void op_div(std::stack<Value>& stack) noexcept;
+static bool op_isunary(SymOp op) noexcept;
+static bool op_isbinary(SymOp op) noexcept;
 
-static SymOp opTable[SYM_COUNT] = {
+static SymOp binopTable[] = {
     op_none,
     op_add,
     op_sub,
-    op_mult,
+    op_mul,
     op_div,
+    NULL,
+};
+
+static SymOp unopTable[] = {
+    NULL,
 };
 
 
@@ -180,18 +183,7 @@ void rpn_print(Rpn *self) noexcept {
     assert(self->stack.size() >= 1);
 
     Value& value = self->stack.top();
-
-    switch (value.type) {
-    case TYPE_FLOAT:
-        std::cout << value.number.f << std::endl;
-        break;
-    case TYPE_INT:
-        std::cout << value.number.u << std::endl;
-        break;
-    case TYPE_UINT:
-        std::cout << value.number.i << std::endl;
-        break;
-    }
+    value.print();
 }
 
 void rpn_free(Rpn *self) noexcept {
@@ -219,10 +211,10 @@ static Node *node_new(char *value) noexcept {
     // one char ops
     else if (value[1] == 0) {
         switch (value[0]) {
-        case '+': return (Node *) new (std::nothrow) SymNode(SYM_ADD);
-        case '-': return (Node *) new (std::nothrow) SymNode(SYM_SUB);
-        case '*': return (Node *) new (std::nothrow) SymNode(SYM_MULT);
-        case '/': return (Node *) new (std::nothrow) SymNode(SYM_DIV);
+        case '+': return (Node *) new (std::nothrow) SymNode(op_add);
+        case '-': return (Node *) new (std::nothrow) SymNode(op_sub);
+        case '*': return (Node *) new (std::nothrow) SymNode(op_mul);
+        case '/': return (Node *) new (std::nothrow) SymNode(op_div);
         }
         assert(0);
     }
@@ -254,7 +246,7 @@ static Node *node_new(char *value) noexcept {
     }
 
     assert(0);
-    return (Node *) new (std::nothrow) SymNode(SYM_NONE);
+    return (Node *) new (std::nothrow) SymNode(op_none);
 }
 
 static void node_free(Node *self) noexcept {
@@ -262,14 +254,24 @@ static void node_free(Node *self) noexcept {
     delete self;
 }
 
-SymNode::SymNode(Symbol value) noexcept :
-    value{value}
+SymNode::SymNode(SymOp op) noexcept :
+    op{op}
 {
 }
 
 void SymNode::exec(std::stack<Value>& stack) noexcept {
     assert(stack.size() >= 1);
-    opTable[this->value](stack);
+
+    if (op_isunary(this->op)) {
+
+    }
+    else {
+        
+    }
+
+    this->op(stack);
+
+    binopTable[this->value](stack);
 }
 
 NumNode::NumNode(Value value) noexcept :
@@ -293,6 +295,7 @@ static void op_add(std::stack<Value>& stack) noexcept {
     stack.pop();
     Value lhs = stack.top();
     stack.pop();
+    lhs.coerce(rhs);
     stack.push(lhs.add(rhs));
 }
 
@@ -304,17 +307,46 @@ static void op_sub(std::stack<Value>& stack) noexcept {
     stack.pop();
     Value lhs = stack.top();
     stack.pop();
+    lhs.coerce(rhs);
     stack.push(lhs.sub(rhs));
 }
 
-static void op_mult(std::stack<Value>& stack) noexcept {
-    assert(stack.size() >= 1);
+static void op_mul(std::stack<Value>& stack) noexcept {
+    size_t size = stack.size();
+    assert(size >= 2);
+
+    Value rhs = stack.top();
+    stack.pop();
+    Value lhs = stack.top();
+    stack.pop();
+    lhs.coerce(rhs);
+    stack.push(lhs.mul(rhs));
 }
 
 static void op_div(std::stack<Value>& stack) noexcept {
-    assert(stack.size() >= 2);
+    size_t size = stack.size();
+    assert(size >= 2);
+
+    Value rhs = stack.top();
+    stack.pop();
+    Value lhs = stack.top();
+    stack.pop();
+    lhs.coerce(rhs);
+    stack.push(lhs.div(rhs));
 }
 
+static bool op_isunary(SymOp op) noexcept {
+    for (size_t i = 0; unopTable[i] != NULL; i++) {
+        if (unopTable[i] == op) {
+            return true;
+        }
+    }
+    return false;
+}
+
+static bool op_isbinary(SymOp op) noexcept {
+    return !op_isunary(op);
+}
 
 Value::Value() noexcept :
     number{0},
@@ -343,66 +375,84 @@ Value::Value(Uint u) noexcept :
     number.u = u;
 }
 
-Value Value::add(Value& other) noexcept {
+void Value::print() noexcept {
     switch (this->type) {
-    case TYPE_FLOAT: switch (other.type) {
-        case TYPE_FLOAT: return Value(this->number.f + other.number.f);
-        case TYPE_INT:   return Value(this->number.f + (Float)other.number.i);
-        case TYPE_UINT:  return Value(this->number.f + (Float)other.number.u);
+    case TYPE_FLOAT:
+        std::cout << this->number.f << std::endl;
+        break;
+    case TYPE_INT:
+        std::cout << this->number.u << std::endl;
+        break;
+    case TYPE_UINT:
+        std::cout << this->number.i << std::endl;
+        break;
     }
-    assert(0);
-    break;
-    case TYPE_INT: switch (other.type) {
-        case TYPE_FLOAT: return Value(this->number.f + other.number.f);
-        case TYPE_INT:   return Value(this->number.i + other.number.i);
-        case TYPE_UINT:  return Value(this->number.u + (Int)other.number.u);
+}
+
+enum Type Value::coerce_chk(Value& other) noexcept {
+    if (this->type < other.type) {
+        return other.type;
     }
-    assert(0);
-    break;
-    case TYPE_UINT: switch (other.type) {
-        case TYPE_FLOAT: return Value(this->number.f + (Float)other.number.f);
-        case TYPE_INT:   return Value(this->number.u + (Uint)other.number.i);
-        case TYPE_UINT:  return Value(this->number.u + other.number.u);
+    return this->type;
+}
+
+void Value::coerce_exec(enum Type type) noexcept {
+    if (this->type >= type) {
+        return;
     }
-    assert(0);
-    break;
+
+    switch (type) {
+    case TYPE_FLOAT: switch (this->type) {
+        case TYPE_INT: this->number.f = (Float)this->number.i; break;
+        case TYPE_UINT: this->number.f = (Float)this->number.u; break;
+        }
+        break;
+    case TYPE_INT: switch (this->type) {
+        case TYPE_FLOAT: this->number.i = (Int)this->number.f; break;
+        case TYPE_UINT: this->number.i = (Int)this->number.u; break;
+        }
+        break;
+    case TYPE_UINT: switch (this->type) {
+        case TYPE_FLOAT: this->number.u = (Uint)this->number.f; break;
+        case TYPE_INT: this->number.u = (Uint)this->number.i; break;
+        }
+        break;
+    default:
+        assert(0);
+        break;
     }
+    this->type = type;
+}
+
+void Value::coerce(Value& other) noexcept {
+    this->coerce_exec(this->coerce_chk(other));
+    other.coerce_exec(other.coerce_chk(*this));
+}
+
+Value Value::add(Value& other) noexcept {
+    assert(this->type == other.type);
+    value_oneop(this, other, +);
     assert(0);
     return Value();
 }
 
 Value Value::sub(Value& other) noexcept {
-    switch (this->type) {
-    case TYPE_FLOAT: switch (other.type) {
-        case TYPE_FLOAT: return Value(this->number.f - other.number.f);
-        case TYPE_INT:   return Value(this->number.f - (Float)other.number.i);
-        case TYPE_UINT:  return Value(this->number.f - (Float)other.number.u);
-    }
-    assert(0);
-    break;
-    case TYPE_INT: switch (other.type) {
-        case TYPE_FLOAT: return Value(this->number.f - other.number.f);
-        case TYPE_INT:   return Value(this->number.i - other.number.i);
-        case TYPE_UINT:  return Value(this->number.u - (Int)other.number.u);
-    }
-    assert(0);
-    break;
-    case TYPE_UINT: switch (other.type) {
-        case TYPE_FLOAT: return Value(this->number.f - (Float)other.number.f);
-        case TYPE_INT:   return Value(this->number.u - (Uint)other.number.i);
-        case TYPE_UINT:  return Value(this->number.u - other.number.u);
-    }
-    assert(0);
-    break;
-    }
+    assert(this->type == other.type);
+    value_oneop(this, other, -);
     assert(0);
     return Value();
 }
 
 Value Value::mul(Value& other) noexcept {
-
+    assert(this->type == other.type);
+    value_oneop(this, other, *);
+    assert(0);
+    return Value();
 }
 
 Value Value::div(Value& other) noexcept {
-
+    assert(this->type == other.type);
+    value_oneop(this, other, /);
+    assert(0);
+    return Value();
 }
